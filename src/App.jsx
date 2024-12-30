a8ea1dbcbd37929ec6b6f0593a27693e5b121ab0
AJIT KUMAR <ajitm.ug21.cs@nitp.ac.in>
2024-12-30 22:48:52 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,25 @@ import Footer from "./Components/Footer/Footer";
 import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
 import Projects from "./Components/Projects/Projects";
-import ChatComponent from "./chatComponent";
+//import ChatComponent from "./chatComponent";
+import ChatbotEmbed from "./ChatbotEmbed";
+//const chatboatId=process.env.REACT_APP_CHATBOT_ID;
 function App() {
+ // console.log(process.env.name);
+  
   return (
     <div className="bg-[#171d32] h-auto w-full overflow-hidden">
       <Navbar />
+        
       <Home />
       <About />
       <Experience />
       <Projects />
-      <ChatComponent/>
+      <ChatbotEmbed chatbotId="1vkxx9h-EnFPLtdCLbeKF"/>
+      {/* <ChatComponent/> */}
       <Footer />
-
+       
+     
     </div>
   );
 }
